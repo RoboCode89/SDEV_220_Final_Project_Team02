20f22530617dfdf077f56ac9aeef48a8247df9a5
Phesse11 <128630819+Phesse11@users.noreply.github.com>
2023-04-15 20:28:43 -0400

    User Input

---
+++ class User:.py
@@ -0,0 +1,15 @@
+class User:
+    def __int__(self, user_id, user_name):
+        self.user_id = user_id
+        self.user_name = user_name
+
+    def adduser():
+
+
+    def editUser():
+    
+    def deleteUser():
+    
+    def searchUser():
+         
+        r
