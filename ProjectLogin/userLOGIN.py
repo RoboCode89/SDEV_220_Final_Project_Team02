8dec1562a207050bf1c48b99b2c98848305e76ef
RoBo41 <99458719+RoBo41@users.noreply.github.com>
2023-04-28 14:08:52 -0400

    Add files via upload
    updated login to have secure passwords

--- ProjectLogin/userLOGIN.py
+++ ProjectLogin/userLOGIN.py
@@ -1,8 +1,11 @@
-
+import hashlib #importing hashing
+#user class to define username and pw
 class User():
     def __init__(self, userName, userPassword):
         self.userName = userName
-        self.userPassword = userPassword
+        #hashing the pw / used sha256 algorithm to has because it is supported by all systems
+        #.encode turns pw to bytes and hexdigest turns the bytes to a hexadecimal number
+        self.userPassword =hashlib.sha256(userPassword.encode()).hexdigest()
 #function for user registration
 def Register():
     userName = input("Username: ")
@@ -15,7 +18,7 @@ def Register():
                 return
     #writing the username and pw to file
     with open("register.txt", "a") as file: #"a"=append credentials to end of text file
-        file.write(userName + '/' + userPassword + "\n")
+        file.write(userName + '/' + hashlib.sha256(userPassword.encode()).hexdigest() + "\n")
 #running function to test
 Register()
 
@@ -29,7 +32,8 @@ def userLogin():
         lines = file.readlines()
         for line in lines:
             items = line.strip().split('/')
-            if items[0] == userName and items[1] == userPassword:
+            #checking if username matches what is stored in file and if the hashed pw matches
+            if items[0] == userName and items[1] == hashlib.sha256(userPassword.encode()).hexdigest():
                 print("Login Success") #sample output               
                 break
         else:
